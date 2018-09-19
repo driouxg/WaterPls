93ab2f2f49c191312a67f7b5cc747cd5319a5a18
Drioux Guidry <drioux.guidry@choicehotels.com>
2018-09-18 19:12:24 -0700

    Inital Commit

---
+++ NewInstaCloneUI/src/InstaClone.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import { View, Text, StyleSheet, Image, Dimensions } from 'react-native';
+
+class InstaClone extends React.Component {
+
+  render() {
+    return ();
+  }
+
+  export default InstaClone;
+}
